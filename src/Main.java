ea6746b9e2416dfd2385f677ff6ba9f64e2d3655
Agnesik <aga.klepka82@gmail.com>
2022-05-19 00:52:52 +0200

    creating a project -- IDE: intelliJ

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Memory Game mi Amigos");
+
+        JFrame frame = new MyFrame("Memory Game");
+
+    }
+}
---
+++ src/MyFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    MyFrame(String name){
+        this.setTitle(name);
+
+
+
+    }
+}
