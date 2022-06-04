21bb59b8860da9ba4bbfa2851a1fee7075605707
Albert Buczak <albert.buczak@bshg.com>
2022-06-04 17:25:03 +0200

    Pull Request review

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args){
-        System.out.println("MEMORY GAME MI AMIGOS");
-
-        GameLogic game = new GameLogic();
-    }
-}
